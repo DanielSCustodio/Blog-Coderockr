53a59bdc68cb6e286bfbf3ef3847afd1cb9b3bc3
Daniel Custodio <play.daniel@hotmail.com>
2022-10-15 16:23:13 -0300

    feat: Global style definition

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return <span>404 - This page could not be found.</span>;
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,33 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
 
+  *{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
   body{
-    background-color: #131313;
+    background: linear-gradient(90deg, #F1A10A 0%, #342303 100%);
+    color: #2D2D2D;
+    font-family: 'Rubik', sans-serif;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  body, input, textarea, select, bottom {
+    font: 400 1.5rem "Rubik";
+    line-height: 2.125rem;
+  }
+
+  h1, h2 {
+    font-size: 2.25rem;
+    line-height: 2.688rem;
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
